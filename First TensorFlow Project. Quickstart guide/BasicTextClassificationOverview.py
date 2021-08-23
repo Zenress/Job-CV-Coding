e98c934219d12b2c0c91e5496df9f31a82c6b86b
Zenress <hotrockgaming@gmail.com>
2021-08-23 09:15:15 +0200

    09:15

--- First TensorFlow Project. Quickstart guide/BasicTextClassificationOverview.py
+++ First TensorFlow Project. Quickstart guide/BasicTextClassificationOverview.py
@@ -17,4 +17,7 @@ dataset = tf.keras.utils.get_file("aclImdb_v1.tar.gz",url,
                                     cache_subdir='.')
 
 dataset_dir = os.path.join(os.path.dirname(dataset), 'aclImdb')
-os.listdir(dataset_dir)
+train_dir = os.path.join(dataset_dir, 'train')
+
+remove_dir = os.path.join(train_dir, 'unsup')
+shutil.rmtree(remove_dir)
