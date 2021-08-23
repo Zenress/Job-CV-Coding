268d0076b0833f074a24a8b8e4f4392d0613de9d
Zenress <hotrockgaming@gmail.com>
2021-08-23 11:39:08 +0200

    11:39

--- First TensorFlow Project. Quickstart guide/BasicTextClassificationOverview.py
+++ First TensorFlow Project. Quickstart guide/BasicTextClassificationOverview.py
@@ -3,7 +3,9 @@ import os
 import re
 import shutil
 import string
+from numpy import vectorize
 import tensorflow as tf
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 from tensorflow.keras import layers
 from tensorflow.keras import losses
@@ -21,3 +23,55 @@ train_dir = os.path.join(dataset_dir, 'train')
 
 remove_dir = os.path.join(train_dir, 'unsup')
 shutil.rmtree(remove_dir)
+
+batch_size = 32
+seed = 42
+
+raw_train_ds = tf.keras.preprocessing.text_dataset_from_directory(
+    'aclImdb/train',
+    batch_size=batch_size,
+    validation_split=0.2,
+    subset='training',
+    seed=seed,
+)
+
+raw_val_ds = tf.keras.preprocessing.text_dataset_from_directory(
+    'aclImdb/train',
+    batch_size=batch_size,
+    validation_split=0.2,
+    subset='validation',
+    seed=seed,
+)
+
+raw_test_ds = tf.keras.preprocessing.text_dataset_from_directory(
+    'aclImdb/test',
+    batch_size=batch_size
+)
+
+def custom_standardization(input_data):
+  lowercase = tf.strings.lower(input_data)
+  stripped_html = tf.strings.regex_replace(lowercase, '<br />', ' ')
+  return tf.strings.regex_replace(stripped_html,'[%s]' % re.escape(string.punctuation), '')
+
+max_features = 10000
+sequence_length = 250
+
+vectorize_layer = TextVectorization(
+  standardize=custom_standardization,
+  max_tokens=max_features,
+  output_mode='int',
+  output_sequence_length=sequence_length
+)
+
+train_text = raw_train_ds.map(lambda x, y: x)
+vectorize_layer.adapt(train_text)
+
+def vectorize_text(text, label):
+  text = tf.expand_dims(text, -1)
+  return vectorize_layer(text), label
+
+text_batch, label_batch = next(iter(raw_train_ds))
+first_review, first_label = text_batch[0], label_batch[0]
+print("Review", first_review)
+print("Label", raw_train_ds.class_names[first_label])
+print("Vectorized Review", vectorize_text(first_review, first_label))
