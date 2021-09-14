ba28511e5e82ba522337278c7075da27a1106b6e
Zenress <hotrockgaming@gmail.com>
2021-09-14 12:42:48 +0200

    12:42

--- First TensorFlow Project. Quickstart guide/ClusteringWithTensorFlow.py
+++ First TensorFlow Project. Quickstart guide/ClusteringWithTensorFlow.py
@@ -4,3 +4,6 @@ logging.getLogger().setLevel(logging.INFO)
 #Switching CPU operation instructions to AVX AVX2
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+import tensorflow_probability as tfp
+import tensorflow as tf
