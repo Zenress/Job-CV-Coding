40ef2ae367959df9db1b17364c5a318cd3d0b3a6
Zenress <hotrockgaming@gmail.com>
2021-10-27 09:23:42 +0200

    09:23

---
+++ PyTorchLearning/PyTorchBuildNeuralNetwork.py
@@ -0,0 +1,9 @@
+import os
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print("Using {} device".format(device))
+
--- PyTorchLearning/PyTorchTensors.py
+++ PyTorchLearning/PyTorchTensors.py
@@ -87,3 +87,27 @@ print(agg_item, type(agg_item))
 print(tensor2, "\n")
 tensor2.add_(5)
 print(tensor2)
+
+#Bridge with Numpy
+#Tensors on the CPU and NumPy arrays can share their underlying memory locations,
+# and changing one will change the other
+
+#Tensor to NumPy array
+t = torch.ones(5)
+print(f"t: {t}")
+n = t.numpy()
+print(f"n: {n}")
+
+#A change in the tensor reflects in the NumPy array
+t.add_(1)
+print(f"t: {t}")
+print(f"n: {n}")
+
+#NumPy array to tensor
+n = np.ones(5)
+t = torch.from_numpy(n)
+
+#Changes in the NumPy array reflects in the tensor
+np.add(n, 1, out=n)
+print(f"t: {t}")
+print(f"n: {n}")
