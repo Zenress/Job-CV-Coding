ed2351478d194bab4e5ad31137520651b0f2fc19
Zenress <hotrockgaming@gmail.com>
2021-11-22 11:48:56 +0100

    11.48

--- S4.2 - Algorithms/4.4 SøgningV2.py
+++ S4.2 - Algorithms/4.4 SøgningV2.py
@@ -1,5 +1,3 @@
-import numpy as np
-from numpy.core.defchararray import array
 
 class SortedArray:
   #Method for calculating the median index of an array
@@ -12,6 +10,9 @@ class SortedArray:
       return round(final_length)
 
   #Binary search method
+  #It searches by checking if the given search number is smaller or bigger than the median
+  #If it is bigger than the median number then the median is used as the start index and we recalculate the median(median)
+  #If it is smaller than the median then the median is used as the end index and we recalculate the median(median) 
   def Search_Array(self,arr,number,start_index, end_index):
     median = self.MedianSearch(start_index, end_index)
     if number == median:
@@ -27,10 +28,36 @@ class SortedArray:
       print(end_index)
       self.Search_Array(arr, number, start_index, end_index)
 
+  #Insert method, used for inserting into a sorted array
+  def Insert(self, insert_nr, array):
+    #Starts by adding the insert_nr to the end of the list
+    i = -1
+    array.append(insert_nr)
+    print(array)
+    #Then we run a for loop that compares the insert_nr to the second last element in the list (Since the last element is the one we inserted)
+    #If the number is smaller than the second last element then it swaps the 2. It does this by assigning the second last element with the last one's value
+    #While holding the second lasts value in a seperate variable (since you can't just swap them as is)
+    for x in range(len(array)):
+      if insert_nr < array[i-1]:   
+        print(array)
+        temp_nr = array[i]             
+        array[i] = array[i-1]
+        array[i-1] = temp_nr             
+        i=i-1
+      if insert_nr == array[i-1]:
+        print(array)
+        break
 
+#Creating an "Array" list with a certain range
+sorted_array = list(range(1,1000))
+
+#Search algorithm part
 number = int(input("Enter a number: "))
-sorted_array = [1,2,3,4,5,6,7,8,9,10]
 print(sorted_array)
 print(type(sorted_array))
 array = SortedArray()
 array.Search_Array(sorted_array, number, 0, sorted_array[-1]) 
+
+#Insert part
+insert_nr = int(input("Enter a number to insert into the SortedArray: "))
+array.Insert(insert_nr, sorted_array)
