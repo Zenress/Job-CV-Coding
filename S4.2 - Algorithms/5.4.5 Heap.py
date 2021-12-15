327d5837269bdb4d12b6c3c6c17d72c0159b838b
Zenress <hotrockgaming@gmail.com>
2021-12-15 13:48:20 +0100

    13:48

--- S4.2 - Algorithms/5.4.5 Heap.py
+++ S4.2 - Algorithms/5.4.5 Heap.py
@@ -6,56 +6,73 @@
 class Heap:
   last_element = None
   root = None
+
   class HeapNode:  
     def __init__(self, data):
       self.left = None
       self.right = None
       self.parent = None
-      self.data = data      
+      self.data = data    
 
   #Insert method for creating nodes
   #Swap upward till the parent value isn't smaller than the current inserted value
   def InsertNode(self,insert_data):
     node = self.last_element    
+    if self.root == None:
+      self.root = self.HeapNode(insert_data)
+      self.last_element = self.root
+      return
     while node.parent != None:
       if node.parent.right == None or node.parent.right != node:
         break
-      node = node.parent
+      node = node.parent    
     if node.parent != None and node.parent.right == None:
       node.parent.right = self.HeapNode(insert_data)
       node.parent.right.parent = node.parent
       self.last_element = node.parent.right
     else:
+      if node.parent != None and node.parent.right != None:
+        node = node.parent.right
       while node.left != None:
         node = node.left
       node.left = self.HeapNode(insert_data)
       node.left.parent = node
       self.last_element = node.left
-    self.Swap_Data(self.last_element)
+    self.Swap_Data()
 
   def Swap_Data(self):
     node = self.last_element
     while node.parent != None and node.data < node.parent.data:
       node.data, node.parent.data = node.parent.data, node.data
       node = node.parent
-  
-  #Method for printing the tree
-  def PrintTree(self,list):
-    if self.left:
-      self.left.PrintTree(list)
-    list.append(self.data),
-    if self.right:
-      self.right.PrintTree(list)
+    if node.parent == None:
+       self.root = node
+    
+  def WriteTree(self,node):
+    self.WriteTree_Rec(node, "")
+
+  def WriteTree_Rec(self,node, indent):
+    if node is not None:
+      print(indent + str(node.data))
+      self.WriteTree_Rec(node.left, indent+"  ")
+      self.WriteTree_Rec(node.right, indent+"  ")
+    else:
+      print(indent+"X")
+
+    """
+    Remove is mirrored of Insert, do at a later day
+    """
 
 heap = Heap()
-heap.InsertNode(10)
-last_element = heap
-heap.InsertNode(last_element,20)
-heap.InsertNode(last_element,15)
-heap.InsertNode(last_element,5)
-heapList = []
-heap.PrintTree(heapList)
-print(heapList)
+heap.InsertNode(7)
+heap.InsertNode(20)
+heap.InsertNode(15)
+heap.InsertNode(5)
+heap.InsertNode(12)
+heap.InsertNode(23)
+heap.InsertNode(28)
+heap.InsertNode(19)
+heap.WriteTree(heap.root)
 # Root = null, Bottom = null
 # ()
 #
